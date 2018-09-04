b68a31bb62ceec033d8d1f6990f45beddaa32855
Joshua-Jenkins <joshjenkins6@yahoo.com>
2018-09-04 10:43:19 -0500

    added express
    add express server to get the dragon generation

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,16 @@
-// const Dragon = require('./dragon.js');
-
-// const fooey = new Dragon({
-//   birthdate: new Date(),
-//   nickname: 'fooey'
-// });
-// const baloo = new Dragon({
-//   birthdate: new Date(),
-//   nickname: 'baloo',
-//   traits: [{
-//     traitsType: 'backgroundColor',
-//     traitValue: 'green'
-//   }]
-// });
-
-// const mimar = new Dragon();
-
-// setTimeout(() => {
-//   const gooby = new Dragon();
-//   console.log('gooby', gooby);
-// }, 3000);
-
-
-
-// console.log('fooey', fooey);
-// console.log('baloo', baloo);
-// console.log('minar', mimar);
-
-// const Generation = require('./generation');
-
-// const generation = new Generation();
-
-// console.log('generation', generation);
-
-// const gooby = generation.newDragon();
-
-// console.log('gooby', gooby);
-
-// setTimeout(() => {
-//   const mimar = generation.newDragon();
-//   console.log('mimar', mimar);
-// }, 15000);
-
+const express = require('express');
 const GenerationEngine = require('./engine');
 
+const app = express();
 const engine = new GenerationEngine();
+const port = 3000;
 
 engine.start();
 
-setTimeout(() => {
-  engine.stop();
-}, 20000);
+app.get('/dragon/new', (req, res) => {
+  res.json({
+    dragon: engine.generation.newDragon()
+  });
+});
+
+app.listen(port, () => console.log(`listening port ${port}`));
